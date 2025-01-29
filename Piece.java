572406df18ec69094f3b4ce2af1fe21edd8c339f
THUSHAN RAJESWARAN <thushan@MacBook-Pro-de-THUSHAN.local>
2025-01-29 18:34:50 +0100

    Modification de Pion.java, Piece.java et ajout de Dame.class

--- Piece.java
+++ Piece.java
@@ -1,15 +1,16 @@
 import java.awt.Color;
 
 public abstract class Piece {
-	protected Color couleur;
-	
-	public Piece(Color couleur) {
-		this.couleur = couleur;
-	}
-	
-	public Color getCouleur() {
-		return couleur;
-	}
-	
-	public abstract boolean peutDeplacer(Case depart, Case arrivee);
+    protected Color couleur;
+    public boolean  estDame = false;
+
+    public Piece(Color couleur) {
+        this.couleur = couleur;
+    }
+    
+    public Color getCouleur() {
+        return couleur;
+    }
+    
+    public abstract boolean peutDeplacer(Case depart, Case arrivee);
 }
